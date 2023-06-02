feaf8411a48d102c50b788c9458c80058c364ed6
Okuhle <okuhlen.20@gmail.com>
2023-06-02 10:47:55 +0000

    update repo

--- js/script.js
+++ js/script.js
@@ -5,43 +5,96 @@ window.addEventListener('DOMContentLoaded', function() {
   var catC3 = document.getElementById('cat-c3');
   var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-  function applyChanges() {
-    // Move cat-shoes to cat-c2
-    catC2.appendChild(catShoes);
+function applyChanges() {
+  // Move cat-shoes to cat-c2
+  catC2.appendChild(catShoes);
 
-    // Add cat-large class and remove cat-medium class from cat-sports
-    catSports.classList.add('cat-large');
-    catSports.classList.remove('cat-medium');
+  // Add cat-large class and remove cat-medium class from cat-sports
+  catSports.classList.add('cat-large');
+  catSports.classList.remove('cat-medium');
 
-    // Move cat-sports to cat-c3
-    catC3.appendChild(catSports);
-  }
+  // Move cat-sports to cat-c3
+  catC3.appendChild(catSports);
+}
 
-  function revertChanges() {
-    // Move cat-shoes back to cat-c3
-    catC3.appendChild(catShoes);
+function revertChanges() {
+  // Move cat-shoes back to cat-c3
+  catC3.appendChild(catShoes);
 
-    // Remove cat-large class and add cat-medium class to cat-sports
-    catSports.classList.remove('cat-large');
-    catSports.classList.add('cat-medium');
+  // Remove cat-large class and add cat-medium class to cat-sports
+  catSports.classList.remove('cat-large');
+  catSports.classList.add('cat-medium');
 
-    // Move cat-sports back to cat-c2
-    catC2.appendChild(catSports);
-  }
+  // Move cat-sports back to cat-c2
+  catC2.appendChild(catSports);
+}
+
+if (screenWidth <= 767) {
+  applyChanges();
+} else {
+  revertChanges();
+}
+
+window.addEventListener('resize', function() {
+  screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
   if (screenWidth <= 767) {
     applyChanges();
   } else {
     revertChanges();
   }
-
-  window.addEventListener('resize', function() {
-    screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-
-    if (screenWidth <= 767) {
-      applyChanges();
-    } else {
-      revertChanges();
-    }
-  });
 });
+});
+
+
+
+// JavaScript code
+
+// Get references to the search button, back button, search screen, and search input
+const searchBtn = document.getElementById('search-btn');
+const backBtn = document.getElementById('back-btn');
+const searchScreen = document.getElementById('search-screen');
+const searchInput = document.getElementById('search-input');
+
+// Add event listeners for click events
+searchBtn.addEventListener('click', showSearchScreen);
+backBtn.addEventListener('click', hideSearchScreen);
+searchInput.addEventListener('input', toggleSearchText);
+
+// Function to show the search screen
+function showSearchScreen() {
+  searchScreen.style.display = 'block';
+  // Add a new history state to prevent the browser from navigating back to the previous page
+  history.pushState({ searchScreenActive: true }, '');
+  searchInput.focus();
+}
+
+// Function to hide the search screen
+function hideSearchScreen() {
+  searchScreen.style.display = 'none';
+  // Go back in history only if the search screen was active
+  if (history.state && history.state.searchScreenActive) {
+    history.back();
+  }
+}
+
+// Function to toggle the search text
+function toggleSearchText() {
+  const searchText = document.getElementById('search-text');
+  if (searchInput.value === '') {
+    searchText.style.display = 'block';
+  } else {
+    searchText.style.display = 'none';
+  }
+}
+
+// Listen for the onpopstate event to handle the back button on mobile devices
+window.onpopstate = function (event) {
+  // Check if the search screen is active and hide it
+  if (searchScreen.style.display === 'block') {
+    searchScreen.style.display = 'none';
+  }
+};
+
+
+
--- js/shop-script.js
+++ js/shop-script.js
@@ -5,16 +5,19 @@ const searchBtn = document.getElementById('search-btn');
 const backBtn = document.getElementById('back-btn');
 const searchScreen = document.getElementById('search-screen');
 const searchInput = document.getElementById('search-input');
+const searchText = document.getElementById('search-text');
 
 // Add event listeners for click events
-searchBtn.addEventListener('click', showSearchScreen);
+searchBtn.addEventListener('click', toggleSearchScreen);
 backBtn.addEventListener('click', hideSearchScreen);
 searchInput.addEventListener('input', toggleSearchText);
 
+// Variable to track search screen visibility
+let isSearchScreenVisible = false;
 
 // Function to toggle the search screen
 function toggleSearchScreen() {
-  if (searchScreen.style.display === 'none') {
+  if (!isSearchScreenVisible) {
     showSearchScreen();
   } else {
     hideSearchScreen();
@@ -23,23 +26,31 @@ function toggleSearchScreen() {
 
 // Function to show the search screen
 function showSearchScreen() {
-  searchScreen.style.display = 'block';
+  searchScreen.style.backgroundColor = 'white';
+  backBtn.style.color = 'grey';
+  searchText.style.display = 'block'
   // Add a new history state to prevent the browser from navigating back to the previous page
   history.pushState({ searchScreenActive: true }, '');
+  searchInput.focus();
+  isSearchScreenVisible = true;
 }
 
 // Function to hide the search screen
 function hideSearchScreen() {
-  searchScreen.style.display = 'none';
+  searchScreen.style.backgroundColor = 'transparent';
+  backBtn.style.color = 'transparent';
+  searchText.style.display = 'none'
   // Go back in history only if the search screen was active
   if (history.state && history.state.searchScreenActive) {
     history.back();
   }
+  isSearchScreenVisible = false;
 }
 
+// Function to toggle the search text
 function toggleSearchText() {
   const searchText = document.getElementById('search-text');
-  if (searchInput.value === '') {
+  if (searchInput.value === '' && isSearchScreenVisible) {
     searchText.style.display = 'block';
   } else {
     searchText.style.display = 'none';
@@ -49,7 +60,36 @@ function toggleSearchText() {
 // Listen for the onpopstate event to handle the back button on mobile devices
 window.onpopstate = function (event) {
   // Check if the search screen is active and hide it
-  if (searchScreen.style.display === 'block') {
-    searchScreen.style.display = 'none';
+  if (isSearchScreenVisible) {
+    hideSearchScreen();
   }
 };
+
+
+
+const filterBtn = document.getElementById('bot-fltr');
+const filterBox = document.getElementById('filter-box');
+const searchHeader = document.getElementById('search-header');
+
+filterBtn.addEventListener('click', () => {
+  filterBtn.style.display = 'none';
+  filterBox.style.transform = 'translateY(100%)';
+  filterBox.style.backgroundColor = 'transparent';
+  filterBox.style.display = 'none';
+  searchHeader.style.backgroundColor = '#FFF';
+});
+
+window.addEventListener('scroll', () => {
+  if (window.pageYOffset > 0) {
+    filterBox.style.transform = 'translateY(-100%)';
+    filterBox.style.backgroundColor = 'transparent';
+    filterBox.style.display = 'none';
+    searchHeader.style.backgroundColor = 'transparent';
+  } else {
+    filterBtn.style.display = 'block';
+    filterBox.style.transform = 'translateY(0)';
+    filterBox.style.backgroundColor = 'initial';
+    filterBox.style.display = 'block';
+    searchHeader.style.backgroundColor = 'initial';
+  }
+});
